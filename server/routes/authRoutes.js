31b321627d66dd668adbf4867f5eaae259025c6a
01masho <masho.practice01@gmail.com>
2025-08-07 16:07:17 +0000

    image upload initiated

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,14 +1,20 @@
 import express from "express";
 import  { loginUser, registerUser } from "../controllers/authController.js";
+import upload from '../middlewares/uploadMiddleware.js';
 
 
 
 const router = express.Router()
 
 router.post('/register', registerUser)
-router.post('/login', loginUser)
-router.post('/upload-image')
-// router.get('/users')
-// router.get('/:id',protect, getUserInfo)
+router.post('/login', loginUser);
+router.post('/upload-image', upload.single("image"), (req, res)=>{
+    if(!req.file){
+        return res.status(400).json({ message: "no file uploaded"});
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`
+    res.status(200).json({imageUrl})
+})
 
-export default router
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
-import express from "express";
+
 import colors from 'colors'
 import cors from "cors";
+import path from 'path'
 import 'dotenv/config'
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
+import { fileURLToPath } from 'url';
+import express from 'express'; // Assuming express is imported elsewhere
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app =express()
 
 app.use(cors({
@@ -18,6 +24,7 @@ const PORT = process.env.PORT || 5000;
 
 //node -e " console.log(require('crypto').randomBytes(64).toString('hex'))"
 app.use('/api/auth', authRoutes)
+app.use("/uploads",express.static(path.join(__dirname, "uploads")));
 app.get('/',async (req, res) => {
     res.send('Hello MERN DEV!')
 })
